b6952fce7449ead4a4f6fb6bda1b6d6a7fbbae29
cirkleDeveloper3 <81671451+DhruvalGorasiya@users.noreply.github.com>
2022-12-27 19:58:03 +0530

    Update loan_status_view.dart

--- lib/view/loan_status_view.dart
+++ lib/view/loan_status_view.dart
@@ -15,22 +15,23 @@ class LoanStatusView extends StatelessWidget {
     return GetBuilder<LoneStatusController>(
       builder: (controller) => Scaffold(
         body: SafeArea(
-          child: Stack(children: [
-            Image.asset('assets/backgrounds/lone_status.png'),
-            controller.data.isEmpty
-                ? Padding(
-                    padding: EdgeInsets.only(top: Get.height * 0.1),
-                    child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          'You have not\napplied yet',
-                          textAlign: TextAlign.center,
-                          style: TextStyleConstant.bold18
-                              .copyWith(color: ColorConstant.white),
-                        )))
-                : Padding(
-                    padding: EdgeInsets.only(top: Get.height * 0.08),
-                    child: Align(
+          child: Stack(
+            children: [
+              Image.asset('assets/backgrounds/lone_status.png'),
+              controller.data.isEmpty
+                  ? Padding(
+                      padding: EdgeInsets.only(top: Get.height * 0.1),
+                      child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Text(
+                            'You have not\napplied yet',
+                            textAlign: TextAlign.center,
+                            style: TextStyleConstant.bold18
+                                .copyWith(color: ColorConstant.white),
+                          )))
+                  : Padding(
+                      padding: EdgeInsets.only(top: Get.height * 0.08),
+                      child: Align(
                         alignment: Alignment.topCenter,
                         child: Text(
                           'If Your application in pending\nstate from last 7 days. Apply\nanother application and email\nus with application number',
@@ -38,26 +39,43 @@ class LoanStatusView extends StatelessWidget {
                           style: TextStyleConstant.bold16
                               .copyWith(fontSize: 15)
                               .copyWith(color: ColorConstant.white),
-                        ))),
-            controller.data.isEmpty
-                ? Center(
-                    child: GestureDetector(
-                        onTap: () {
-                          Get.toNamed(Routes.loanDetailView);
-                        },
-                        child: Image.asset(ImageConstant.applyNow,
-                            width: Get.width * 0.35)),
-                  )
-                : ListView.builder(
-                    itemCount: controller.data.length,
-                    padding: EdgeInsets.only(
-                        top: Get.width * 0.57,
-                        left: Get.width * 0.02,
-                        right: Get.width * 0.02),
-                    itemBuilder: (context, index) {
-                      return Column(
-                        children: [
-                          if (index == 0)
+                        ),
+                      ),
+                    ),
+              controller.data.isEmpty
+                  ? Center(
+                      child: GestureDetector(
+                          onTap: () {
+                            Get.toNamed(Routes.loanDetailView);
+                          },
+                          child: Image.asset(ImageConstant.applyNow,
+                              width: Get.width * 0.35)),
+                    )
+                  : ListView.builder(
+                      itemCount: controller.data.length,
+                      padding: EdgeInsets.only(
+                          top: Get.width * 0.57,
+                          left: Get.width * 0.02,
+                          right: Get.width * 0.02),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            if (index == 0)
+                              Padding(
+                                padding:
+                                    EdgeInsets.only(bottom: Get.width * 0.02),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    _text(flex: 2, text: 'Sr.No', isHead: true),
+                                    _text(
+                                        flex: 4, text: 'Appl.No', isHead: true),
+                                    _text(flex: 3, text: 'Date', isHead: true),
+                                    _text(flex: 3, text: 'Status', isHead: true)
+                                  ],
+                                ),
+                              ),
                             Padding(
                               padding:
                                   EdgeInsets.only(bottom: Get.width * 0.02),
@@ -65,39 +83,28 @@ class LoanStatusView extends StatelessWidget {
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceEvenly,
                                 children: [
-                                  _text(flex: 2, text: 'Sr.No', isHead: true),
-                                  _text(flex: 4, text: 'Appl.No', isHead: true),
-                                  _text(flex: 3, text: 'Date', isHead: true),
-                                  _text(flex: 3, text: 'Status', isHead: true)
+                                  _text(
+                                      flex: 2,
+                                      text: (index + 1).toString(),
+                                      isHead: false),
+                                  _text(
+                                      flex: 4,
+                                      text: controller.data[index]['id'],
+                                      isHead: false),
+                                  _text(
+                                      flex: 3,
+                                      text: controller.data[index]['date'],
+                                      isHead: false),
+                                  _text(flex: 3, text: 'Pending', isHead: false)
                                 ],
                               ),
                             ),
-                          Padding(
-                            padding: EdgeInsets.only(bottom: Get.width * 0.02),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                _text(
-                                    flex: 2,
-                                    text: (index + 1).toString(),
-                                    isHead: false),
-                                _text(
-                                    flex: 4,
-                                    text: controller.data[index]['id'],
-                                    isHead: false),
-                                _text(
-                                    flex: 3,
-                                    text: controller.data[index]['date'],
-                                    isHead: false),
-                                _text(flex: 3, text: 'Pending', isHead: false)
-                              ],
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  )
-          ]),
+                          ],
+                        );
+                      },
+                    ),
+            ],
+          ),
         ),
       ),
     );
@@ -111,10 +118,11 @@ class LoanStatusView extends StatelessWidget {
           child: Text(text,
               style: isHead
                   ? TextStyleConstant.bold18
-                      .copyWith(color: ColorConstant.black,fontSize: 16)
+                      .copyWith(color: ColorConstant.black, fontSize: 16)
                   : TextStyleConstant.bold16.copyWith(
                       color: ColorConstant.black,
-                      fontWeight: FontWeight.normal,fontSize: 14)),
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14)),
         ));
   }
 }
